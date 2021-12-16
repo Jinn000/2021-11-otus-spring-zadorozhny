31d8228325a5977305d8d6aecee08282159a8f68
Andrey <andrey@none.ru>
2021-12-17 02:06:21 +0300

    IO service refactoring with constructor

--- students-examination/src/main/java/org/zav/iu/ConsoleLayoutServiceImpl.java
+++ students-examination/src/main/java/org/zav/iu/ConsoleLayoutServiceImpl.java
@@ -1,23 +1,31 @@
 package org.zav.iu;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.io.InputStream;
+import java.io.PrintStream;
 import java.util.Scanner;
 
 /**Сервис для вывода данных на экран*/
 @Service
 public class ConsoleLayoutServiceImpl implements LayoutService<String, String> {
-    private final Scanner scanner = new Scanner(System.in);
+    private final Scanner scanner;
+    private final PrintStream printStream;
 
+    public ConsoleLayoutServiceImpl(@Value("#{ T(java.lang.System).in}") InputStream inputStream, @Value("#{ T(java.lang.System).out}") PrintStream printStream) {
+        this.scanner = new Scanner(inputStream);
+        this.printStream = printStream;
+    }
 
     @Override
     public void show(String content) {
-        System.out.println(content);
+        printStream.println(content);
     }
 
     @Override
     public String ask(String content) {
-        System.out.println(content);
+        printStream.println(content);
         return scanner.next();
     }
 }
