e3bd26a994dead3c1e1122a5e7fa82185f50051a
Andrey <andrey@none.ru>
2022-01-11 00:46:54 +0300

    start

---
+++ stored-books-info/src/main/java/ru/zav/StoredBooksInfoApplication.java
@@ -0,0 +1,13 @@
+package ru.zav;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoredBooksInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoredBooksInfoApplication.class, args);
+	}
+
+}
